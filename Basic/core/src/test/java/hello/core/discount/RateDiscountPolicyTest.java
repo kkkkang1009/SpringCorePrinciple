635947c0797bf048098154575f15a579bc61f9ff
kkkkang1009 <kkkkkkkkang@nate.com>
2023-01-28 15:46:15 +0900

    Basic, 새로운 할인 정책 개발

---
+++ Basic/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,34 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP 10% discount")
+    void vip_o(){
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        int discount = discountPolicy.discount(member, 10000);
+
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("Not VIP no discount")
+    void vip_x(){
+        Member member = new Member(1L, "memberBASIC", Grade.BASIC);
+
+        int discount = discountPolicy.discount(member, 10000);
+
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
+}
