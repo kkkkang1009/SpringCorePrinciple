811171fbf7c154fc6de8f9af815762fc0ef7c96a
kkkkang1009 <kkkkkkkkang@nate.com>
2023-02-12 22:37:20 +0900

    BASIC, 스프링 컨테이너와 스프링 빈

---
+++ Basic/core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,30 @@
+package hello.core.beandefinition;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BeanDefinitionTest {
+    // annotation 방식은 factory method를 이용하여 만드는 방식
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+//    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if( beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                System.out.println("beanDefinitionName = " + beanDefinitionName +
+                        " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
